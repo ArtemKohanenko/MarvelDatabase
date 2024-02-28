51be0f7f50fcfffaf9862621771b1ad8e8f14597
ArtemKohanenko <kartem0255@gmail.com>
2024-02-28 19:44:51 +0600

    Add routing and Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { RouteObject, useRoutes } from "react-router-dom";
 import Header from "./components/Header/Header"
+import Layout from "./components/Layout/Layout";
 
 function App() {
 
+  let routes: RouteObject[] = [
+    {
+      path: "/",
+      element: <Layout/>,
+      children: [
+      ],
+    },
+  ];
+
+
+  let element = useRoutes(routes)
+
   return (
     <>
+      {element}
     </>
   )
 }
