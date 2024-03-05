42cf715c06786a990642c1b3a79af0057a4edf0f
ArtemKohanenko <kartem0255@gmail.com>
2024-03-05 22:32:11 +0600

    Work on Footer component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const Layout = () => {
 
@@ -7,7 +8,9 @@ const Layout = () => {
         <>
             <Header/>
             
-            <Outlet/>
+                <Outlet/>
+
+            <Footer/>
         </>
     )
 }
