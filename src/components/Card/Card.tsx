a9a61991b48281a81664f08ad123ab53ed1c8c92
ArtemKohanenko <kartem0255@gmail.com>
2024-03-06 22:16:49 +0600

    Add character detail page

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,21 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import classes from './Card.module.scss'
 import { ICharacter } from '../../types/character';
 import { IListable } from '../../types/IListable';
 
 const Card = (props: {item: IListable}) => {
-
+    
     const item = props.item;
+
+    const navigate = useNavigate();
+
+    const clickHandler = () => {
+        navigate(item.id)
+    }
     
     return (
         <>
-            <div className={classes.container}>
+            <div className={classes.container} onClick={clickHandler}>
                 <div className={classes.pictureContainer}>
                     <img src={item.picture} className={classes.picture}></img>
                 </div>
