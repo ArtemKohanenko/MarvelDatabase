7413849f7678190456a95ba793c6b121135771ba
ArtemKohanenko <kartem0255@gmail.com>
2024-03-06 14:26:19 +0600

    Push Footer to bottom of page

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import classes from './Layout.module.scss'
 
 const Layout = () => {
 
     return (
         <>
+        <div className={classes.wrapper}>
             <Header/>
             
+            <main>
                 <Outlet/>
-
+            </main>
+                        
             <Footer/>
+        </div>
+            
         </>
     )
 }
