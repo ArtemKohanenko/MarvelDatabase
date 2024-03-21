93184b9666f9f13f0d5b02b39570ebaf80b3162a
ArtemKohanenko <kartem0255@gmail.com>
2024-03-21 17:55:31 +0600

    Add comics store and comics api, change comics types

--- src/routes/ComicDetail/ComicDetail.tsx
+++ src/routes/ComicDetail/ComicDetail.tsx
@@ -1,32 +1,45 @@
 import { useParams } from "react-router-dom";
 import classes from "./ComicDetail.module.scss";
+import { useEffect } from "react";
+import comicsStore from "../../stores/ComicsStore";
 
 const ComicDetail = () => {
   const { id } = useParams();
+  const { selectedComic, getComicById } = comicsStore;
+  const pictureURI =
+    selectedComic?.thumbnail.path + "." + selectedComic?.thumbnail.extension;
+
+  useEffect(() => {
+    if (id) {
+      getComicById(id);
+    }
+  });
 
   return (
     <>
       <div className={classes.container}>
         <div className={classes.pictureContainer}>
-          <img src="" className={classes.picture}></img>
+          <img src={pictureURI} className={classes.picture}></img>
         </div>
         <div className={classes.textData}>
           <div className={classes.leftColumn}>
             <span className={classes.name}>{}</span>
-            <span className={classes.description}>{}</span>
+            <span className={classes.description}>
+              {selectedComic?.description}
+            </span>
           </div>
           <div className={classes.rightColumn}>
             <span className={classes.title}>Characters</span>
-            {/* {comic.charactersLinks.map((item) => (
+            {selectedComic?.characters.items.map((item) => (
               <a
                 target="_blank"
                 rel="noopener noreferrer"
-                href={item.link}
+                href={item.resourceURI}
                 className={classes.link}
               >
                 {item.name}
               </a>
-            ))} */}
+            ))}
           </div>
         </div>
       </div>
