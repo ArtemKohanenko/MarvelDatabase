b476377aa04f434d910b19449e9836f11482c01e
ArtemKohanenko <kartem0255@gmail.com>
2024-04-04 22:40:53 +0600

    Sort favourite cards by name/title

--- src/routes/Favourites/Favourites.tsx
+++ src/routes/Favourites/Favourites.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import { IListable } from "../../types/IListable";
 
 const Favourites = () => {
-  const pageSize = 18;
   const [currentPage, setCurrentPage] = useState(0);
 
   const charactersFavourites: IListable[] = JSON.parse(
@@ -14,8 +13,21 @@ const Favourites = () => {
     localStorage.getItem("comicsFavourites") ?? "[]",
   );
   const favouritesList = charactersFavourites.concat(comicsFavourites);
+  favouritesList.sort((a, b) => {
+    const aName = a.name ? a.name : a.title!;
+    const bName = b.name ? b.name : b.title!;
+    if (aName < bName) {
+      return -1
+    }
+    else if (aName > bName) {
+      return 1
+    }
+    else {
+      return 0;
+    }
+  })
   const amount = favouritesList.length;
-  const pagesAmount = Math.ceil(amount / pageSize);
+  const pagesAmount = 1;
 
   return (
     <>
