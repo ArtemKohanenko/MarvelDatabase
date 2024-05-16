d536f192ef88a970316e8fa086f2c045997fea0b
ArtemKohanenko <kartem0255@gmail.com>
2024-05-16 20:52:38 +0600

    Implement push notifications

---
+++ src/components/Message/Message.tsx
@@ -0,0 +1,23 @@
+import { NotificationPayload } from 'firebase/messaging';
+import classes from './Message.module.scss'
+
+const Message = (props: { notification?: NotificationPayload }) => {
+  const notification = props.notification;
+  
+  return (
+    <>
+      <div className={classes.notificationHeader}>
+        {notification?.image && (
+          <div className={classes.imageContainer}>
+            <img src={notification.image} width={100} />
+          </div>
+        )}
+        <span>{notification?.title}</span>
+      </div>
+      <div className={classes.notificationBody}>{notification?.body}</div>
+    </>
+  );
+};
+  
+  export default Message;
+  
